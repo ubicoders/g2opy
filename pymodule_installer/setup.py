4d099479ce1b500a46f625c387c12a061fefa900
root <anon@ubicoders.com>
2024-05-12 02:35:22 +0000

    updated

--- pymodule_installer/setup.py
+++ pymodule_installer/setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
+
+# read the contents of your README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='g2opy',
-    version='0.0.1',
+    version='0.0.3',
     package_dir={'g2opy': 'g2opy'},
     packages=find_packages(),
     include_package_data=True,
-    description='Simple package containing a shared object library',
-    author='Your Name',
-    author_email='your.email@example.com'
+    description='g2o for python',
+    author='ubicoders',
+    keywords='g2o, SLAM, BA, ICP, optimization, python, binding',
+    author_email='info@ubicoders.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important for Markdown files
 )
